8b6ffd020143904c12ed5a4bdc669d6f8ab61aad
yasRF <yas070788@hotmail.es>
2022-10-27 16:52:18 +0000

    mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+const Home =() =>{
+	const [newItem, setNewItem] = useState("");
+	const [items, setItems] = useState([]);
+
+
+	function addItem() {
+	const item= {
+		id: Math.floor(Math.random() * 1000),
+		value: newItem
+	};
+	setItems(oldList => [...oldList, item]);
+	setNewItem("");
+ }
+ function deleteItem(id){
+	const newArray = items.filter(item => item.id !==id);
+	setItems(newArray);
+
+ }
+ 
+
+
+  return(
+	<div className="App">
+		<h1>Lista de tareas</h1>
+		<input type="text" placeholder='Add an item...' value={newItem}
+		onChange={e => setNewItem(e.target.value)}
+		/>
+		<button onClick={() => addItem()}>Crear</button>
+		<ul>
+			{items.map(item => {
+				return(
+					<li key={item.id}>{item.value}<button onClick={() => deleteItem(item.id)}>Eliminar</button></li>
+				)
+			})}
+		</ul>
+	</div>
+  );
+		};
+
+
+
 
 export default Home;
